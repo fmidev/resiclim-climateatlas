ec8cea81c38ce47f2e89e7c52e607052262c27d4
Matti Kamarainen <kamarain@puhti-login1.bullx>
2022-01-25 18:27:36 +0200

    Allas procedures updated

---
+++ download_era5_land_nc.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+#
+#
+
+import os, sys, itertools
+import numpy as np
+import xarray as xr
+
+import cdsapi
+server = cdsapi.Client()
+
+
+sys.path.append('/users/kamarain/resiclim-climateatlas')
+import allas_utils as allas
+
+
+varname = str(sys.argv[1]) 
+
+
+years = np.arange(1950,2022).astype(int)
+years = np.arange(2021,1949,-1).astype(int)
+years = np.arange(2021,2015,-1).astype(int)
+#years = (2020,)
+months = np.arange(1,13)
+
+for year, month in itertools.product(years, months):
+    
+    basename = '%s_era5Land_%04d%02d' % (varname,year,month)
+    nc_file  = '%s.nc'  % (basename)
+    zr_file  = '%s.zarr'  % (basename)
+    remote_path = 'resiclim/coarse/'+varname+'/'+nc_file
+    
+    if allas.isfile_s3(remote_path):
+        pass
+    
+    else:
+        
+        opts = {
+                'product_type'  : 'reanalysis',  
+                'area'          : [90, -180, 45, 180,],
+                'variable'      : varname, 
+                'year'          : '%04d' % (year),
+                'month'         : '%02d' % (month),
+                'day'           : ['%02d' % (i+1) for i in range(31)], 
+                'time'          : ['%02d:00' % (i) for i in range(24)], 
+                'grid'          : '0.5/0.5', #'0.125/0.125',
+                'format'        : 'netcdf',
+               }
+        
+        print('Fetching data for', nc_file)
+        
+        # Retrieve from Climate Data Store
+        try:
+            server.retrieve('reanalysis-era5-land', opts, nc_file)
+        except:
+            print('Retrieval failed for',nc_file)
+            
+        
+        # Upload to S3 Allas and remove the temporal file from disk
+        try:
+            allas.upload_data_s3(remote_path, nc_file)
+            #ds = xr.open_dataset(nc_file)
+            #task = allas.xr_to_zarr_s3(ds, remote_path)
+            os.remove(nc_file)
+        except:
+            print('Upload failed for',nc_file)
+
--- download_era5_land_zr.py
+++ download_era5_land_zr.py
@@ -4,9 +4,13 @@
 
 import os, sys, itertools
 import numpy as np
+import xarray as xr
+
 import cdsapi
 server = cdsapi.Client()
 
+
+sys.path.append('/users/kamarain/resiclim-climateatlas')
 import allas_utils as allas
 
 
@@ -15,14 +19,17 @@ varname = str(sys.argv[1])
 
 years = np.arange(1950,2022).astype(int)
 years = np.arange(2021,1949,-1).astype(int)
-#years = (2020,)
+years = np.arange(2021,2015,-1).astype(int)
+years = (2020,)
 months = np.arange(1,13)
 
-for month, year in itertools.product(months,years):
+for year, month in itertools.product(years, months):
     
     basename = '%s_era5Land_%04d%02d' % (varname,year,month)
     nc_file  = '%s.nc'  % (basename)
-    remote_path = 'resiclim/'+varname+'/'+nc_file
+    zr_file  = '%s.zarr'  % (basename)
+    
+    remote_path = 's3://resiclim/test/'+varname+'/'+zr_file
     
     if allas.isfile_s3(remote_path):
         pass
@@ -37,6 +44,7 @@ for month, year in itertools.product(months,years):
                 'month'         : '%02d' % (month),
                 'day'           : ['%02d' % (i+1) for i in range(31)], 
                 'time'          : ['%02d:00' % (i) for i in range(24)], 
+                #'grid'          : '0.5/0.5', #'0.125/0.125',
                 'format'        : 'netcdf',
                }
         
@@ -51,7 +59,8 @@ for month, year in itertools.product(months,years):
         
         # Upload to S3 Allas and remove the temporal file from disk
         try:
-            allas.upload_data_s3(remote_path, nc_file)
+            ds = xr.open_dataset(nc_file)
+            task = allas.xr_to_zarr_s3(ds, remote_path)
             os.remove(nc_file)
         except:
             print('Upload failed for',nc_file)
